3cac8b9e78b58ab259761cc813f8d621ce7c9864
Willian S <wsapcf@gmail.com>
2022-01-19 21:41:12 -0300

    add test: should call httpClient with correct body

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,4 +1,5 @@
 import 'package:faker/faker.dart';
+import 'package:fordevs/domain/usecases/usecases.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
@@ -8,13 +9,18 @@ class RemoteAuthentication {
   final String url;
 
   RemoteAuthentication({@required this.httpClient, @required this.url});
-  Future<void> auth() async {
-    await httpClient.request(url: url, method: 'post');
+  Future<void> auth(AuthenticationParams params) async {
+    final body = {
+      'email': params.email,
+      'password': params.secret,
+    };
+    await httpClient.request(url: url, method: 'post', body: body);
   }
 }
 
 class HttpClient {
-  Future<void> request({@required String url, @required String method}) async {}
+  Future<void> request(
+      {@required String url, @required String method, Map body}) async {}
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
@@ -23,25 +29,44 @@ void main() {
   HttpClientSpy httpClient;
   String url;
   RemoteAuthentication sut;
+  AuthenticationParams params;
   setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
+    params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
   });
   test('should call httpClient with correct url', () async {
     //arrange
 
     //act
-    await sut.auth();
+    await sut.auth(params);
     //assert
-    verify(httpClient.request(url: url, method: 'post'));
+    verify(httpClient.request(
+        url: url,
+        method: 'post',
+        body: {"email": params.email, "password": params.secret}));
   });
   test('should call httpClient with correct values', () async {
     //arrange
+    //act
+    await sut.auth(params);
+    //assert
+    verify(httpClient.request(
+        url: url,
+        method: 'post',
+        body: {"email": params.email, "password": params.secret}));
+  });
+  test('should call httpClient with correct body', () async {
+    //arrange
 
     //act
-    await sut.auth();
+    await sut.auth(params);
     //assert
-    verify(httpClient.request(url: url, method: 'post'));
+    verify(httpClient.request(
+        url: url,
+        method: 'post',
+        body: {'email': params.email, 'password': params.secret}));
   });
 }
