0a294d44b3940d6e76d24daf7d9ffe95d2793f6d
Willian S <wsapcf@gmail.com>
2022-02-02 20:55:33 -0300

    add url

--- test/infra/http_client_test.dart
+++ test/infra/http_client_test.dart
@@ -1,10 +1,11 @@
+import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
 class HttpAdapter {
-  final Client client;
+  final ClientSpy client;
 
   HttpAdapter(this.client);
   Future<void> request({@required String url, @required String method}) async {}
@@ -18,8 +19,9 @@ void main() {
       //arrange
       final client = ClientSpy();
       final sut = HttpAdapter(client);
+      final url = faker.internet.httpUrl();
       //act
-
+      await sut.request(url: url, method: 'post');
       //assert
     });
   });
