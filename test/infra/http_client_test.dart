512fba9eec436d592e39bfda283d5bc09215cde1
Willian S <wsapcf@gmail.com>
2022-02-02 21:10:28 -0300

    add httpadapter body test

--- test/infra/http_client_test.dart
+++ test/infra/http_client_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -8,21 +10,65 @@ class HttpAdapter {
   final ClientSpy client;
 
   HttpAdapter(this.client);
-  Future<void> request({@required String url, @required String method}) async {}
+  Future<void> request(
+      {@required String url, @required String method, Map body}) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    final jsonBody = body != null ? json.encode(body) : null;
+    await client.post(url, headers: headers, body: jsonBody);
+  }
 }
 
 class ClientSpy extends Mock implements Client {}
 
 void main() {
+  ClientSpy client;
+  HttpAdapter sut;
+  String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+  });
   group('POST method', () {
     test('should call POST with correct values', () async {
       //arrange
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
-      final url = faker.internet.httpUrl();
+
       //act
       await sut.request(url: url, method: 'post');
       //assert
+      verify(client.post(url, headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      }));
+    });
+    test('should pass json content type in headers', () async {
+      //arrange
+      //act
+      await sut.request(url: url, method: 'post');
+      //assert
+      verify(client.post(url, headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      }));
+    });
+    test('should call POST with correct body', () async {
+      //arrange
+
+      //act
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
+      //assert
+      verify(client.post(url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+          },
+          //stringified
+          body: '{"any_key":"any_value"}'));
     });
   });
 }
